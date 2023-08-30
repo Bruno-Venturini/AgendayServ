cb73875f8b63b98a26c91b21acb38f308565f1c7
Bruno-Venturini <brunofreloventurini@outlook.com>
2023-08-29 21:59:18 -0300

    Inicializando dependências do projeto

---
+++ src/main/java/com/agenday/agendayserv/AgendayservApplication.java
@@ -0,0 +1,13 @@
+package com.agenday.agendayserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendayservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendayservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenday/agendayserv/AgendayservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenday.agendayserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendayservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
