f673b84aa8a531122e896b4b3e0673c8001f6c4e
higor.massiroli <higor.massiroli@sistemainfo.com.br>
2023-10-16 20:03:17 -0300

    add modelMapping

--- src/main/java/com/agenday/agendayserv/utils/StringUtils.java
+++ src/main/java/com/agenday/agendayserv/utils/StringUtils.java
@@ -3,7 +3,11 @@ package com.agenday.agendayserv.utils;
 import java.lang.String;
 
 public class StringUtils {
-  public static boolean isNullOrEmpty(String value) {
-    return value == null || value.trim().isEmpty();
-  }
+    public static boolean isNullOrEmpty(String value) {
+        return value == null || value.trim().isEmpty();
+    }
+
+    public static String remove(String value, String target) {
+        return value.replace(target, "");
+    }
 }
